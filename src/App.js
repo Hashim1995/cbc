9f26bc8cbab7e03028859129dc697c3ece284c97
Hashim Hashimli <hashimli.hshm@gmail.com>
2021-11-25 13:02:34 +0400

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./Assets/global.scss";
+import "antd/dist/antd.css";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      {/* <Footer /> */}
     </div>
   );
 }
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,25 @@
+import Style from "./Header.module.scss";
+import logo from "../../Assets/Img/logo.webp";
+import { Link } from "react-router-dom";
+const Header = () => {
+  return (
+    <div className="container">
+      <nav className={Style.nav}>
+        <Link to="/">
+          <img src={logo} alt="" />
+        </Link>
+        <div>
+          <Link to="/">HOME</Link>
+          <Link to="/">ABOUT</Link>
+          <Link to="/">SERVICES</Link>
+          <Link to="/">PROJECTS</Link>
+          <Link to="/">BLOG</Link>
+          <Link to="/">PARTNERS</Link>
+          <Link to="/">CONTACT</Link>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
